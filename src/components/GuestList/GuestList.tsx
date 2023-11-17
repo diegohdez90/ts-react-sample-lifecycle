797709bc642eb86f74b9883deb31df5196ee8e43
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-11-17 11:50:25 -0600

    feat: add insert to list functionallity
    feat: clean after insert

--- src/components/GuestList/GuestList.tsx
+++ src/components/GuestList/GuestList.tsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 const GuestList: React.FC = () => {
   const [name, setName] = useState('');
+  const [list, setList] = useState<string[]>([]);
+  const cleanInput = useCallback(() => setName(''), [])
 
   const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
   }
-  
+
+  const addToList = () => {
+    setList(prev => [...prev, name]);
+    cleanInput();
+  }
+
   return (
     <div>
       <h3>GuestList</h3>
@@ -15,7 +22,14 @@ const GuestList: React.FC = () => {
         value={name}
         onChange={onChangeName}
       />
-      <button>Add guest</button>
+      <button
+        onClick={addToList}
+      >Add guest</button>
+      <ul>
+        {
+          list.map(item => <li>{item}</li>)
+        }
+      </ul>
     </div>
   )
 }
